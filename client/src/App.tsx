baeeca30364ae5b2b9c8da84161dddc22d86afad
leith ayadi <leithayadi188@gmail.com>
2023-10-10 03:08:57 -1100

    leith

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,17 @@
-import React ,{useEffect}  from 'react';
-import { useSelector, useDispatch } from 'react-redux'
-import { fetchData,addData } from './store/test'
-import type { RootState,AppDispatch } from './store/index'
-import PassengerInfo from './Pages/PassengerInfo';
-
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { fetchData, addData } from './store/test';
+import type { RootState, AppDispatch } from './store/index';
+import { BrowserRouter , Routes, Route } from 'react-router-dom';
+import SignUp from './Components/SignUp';
 
 
 function App() {
   return (
-    <div>
-          {/* <h1>hello</h1> */}
-          <PassengerInfo />
-    </div> 
+    <BrowserRouter>
+    <Routes></Routes>
+      <Route path="/singup"  element={<SignUp />}/>
+    </BrowserRouter>
   );
 }
 
