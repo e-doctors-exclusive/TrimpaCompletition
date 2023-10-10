8b27d6db88b446d238da365fdae8efdc8d1b2674
Jesserbenkhiria <jesserbenkhiria911@gmail.com>
2023-10-10 13:15:33 +0100

    setup

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,16 @@
-import React  from 'react';
+import React ,{useEffect}  from 'react';
+import { useSelector, useDispatch } from 'react-redux'
+import { fetchData,addData } from './store/test'
+import type { RootState,AppDispatch } from './store/index'
+
 
 
 function App() {
+
   return (
-    <div className="App">
-      <h1>hello</h1>
-    </div>
+    <div>
+          <h1>hello</h1>
+    </div> 
   );
 }
 
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,11 +1,16 @@
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { store } from './store/index'
+import { Provider } from 'react-redux'
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
+  <Provider store={store} >
     <App />
+  </Provider>
+    
 );
 
 // If you want to start measuring performance in your app, pass a function
