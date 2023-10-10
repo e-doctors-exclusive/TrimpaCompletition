967cf498cdbf72ee3066379aff4a8fde9c07fdbc
FediMaalej <fm3aalej@gmail.com>
2023-10-10 14:33:39 +0100

    fedi

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,7 @@ import React ,{useEffect}  from 'react';
 import { useSelector, useDispatch } from 'react-redux'
 import { fetchData,addData } from './store/test'
 import type { RootState,AppDispatch } from './store/index'
+import PassengerInfo from './Pages/PassengerInfo';
 
 
 
@@ -9,7 +10,8 @@ function App() {
 
   return (
     <div>
-          <h1>hello</h1>
+          {/* <h1>hello</h1> */}
+          <PassengerInfo />
     </div> 
   );
 }
