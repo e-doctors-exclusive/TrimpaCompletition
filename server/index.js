3bfa79bc31433d7852f93f9465aea353f4479361
leith ayadi <leithayadi188@gmail.com>
2023-10-11 05:40:16 -1100

    back admin

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const cors = require('cors')
 const app = express();
 const port = 1128; // You can choose any available port
 const userRoutes = require("./router/user.routes.js")
+const adminRoutes = require("./router/admin.router.js")
 require("./database/index.js")
 app.use(express.json());
 app.use(cors())
@@ -10,6 +11,7 @@ app.use(cors())
 
 
 app.use("/users", userRoutes)
+app.use("/admin", adminRoutes)
 // Start the server
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
