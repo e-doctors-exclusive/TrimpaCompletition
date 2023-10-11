cec0dae94884fe172813fe51717546ec4d99a2a9
FediMaalej <fm3aalej@gmail.com>
2023-10-11 16:29:40 +0100

    modal test

--- client/src/Pages/ProfileUser.tsx
+++ client/src/Pages/ProfileUser.tsx
@@ -2,6 +2,7 @@ import "../styles/ClientProfil.css";
 import React from "react";
 import Navbar from "../Components/Navbar";
 import Footer from "../Components/Footer";
+import Avatar from "../Assets/avatar.jpeg"
 
 const ProfileUser: React.FC = () => {
   return (
@@ -23,7 +24,33 @@ const ProfileUser: React.FC = () => {
               <p className="account_seeting_desc">Details about your Personal information</p>
             </div>
           </div>
-          <div className="profile_container"></div>
+          <div className="profile_container">
+            <div className="profile_img">
+              <div className="avatar_container">
+                <img src={Avatar} alt="" id="avatar" />
+                <div className="avatar_info">
+                  <p id="avatar_title">Upload a New Photo</p>
+                  <p id="avatar_file_source">Profile-pic.jpg</p>
+                </div>
+              </div>
+              <button>Update</button>
+            </div>
+            <div className="profile_information">
+              <p>Change User Information here</p>
+              <div className="username_email">
+                <input type="text" placeholder="username" />
+                <input type="text" placeholder="email" />
+              </div>
+              <input type="text" placeholder="Adress*" />
+              <div className="user_info">
+                <input type="text" placeholder="City" />
+                <input type="text" placeholder="State/Province" />
+                <input type="text" placeholder="Zip Code" />
+                <input type="text" placeholder="Country" />
+              </div>
+              <button>Update Information</button>
+            </div>
+          </div>
         </div>
       </div>
       {/* <Footer /> */}
