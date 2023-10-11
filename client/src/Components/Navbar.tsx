ebd9dd1e11d065a76bc3b1d9dc4c73d10ea18c41
FediMaalej <fm3aalej@gmail.com>
2023-10-11 14:20:32 +0100

    sign up completed

--- client/src/Components/Navbar.tsx
+++ client/src/Components/Navbar.tsx
@@ -1,8 +1,12 @@
 import "../styles/Navbar.css";
+import "../styles/SignUp.css";
 import React, { useState } from "react";
 import logo from "../Components/Logo";
 import Logo from "../Components/Logo";
 import SignUp from "../Pages/SignUp";
+import googleIcon from "../Assets/icons/thirdPartyIcons/color.svg";
+import appleIcon from "../Assets/icons/thirdPartyIcons/appleMac.svg";
+import facebookIcon from "../Assets/icons/thirdPartyIcons/facebook.svg";
 
 const Navbar = () => {
   const [isModalOpen, setModalOpen] = useState(false);
@@ -32,7 +36,53 @@ const Navbar = () => {
         </button>
       </div>
       <SignUp isOpen={isModalOpen} onClose={closeModal}>
-        
+        <form className="main_signUp_container">
+          <div className="signUp_header">
+            <div className="signUp_desc">
+              <p id="signUp_title">Sign up for Tripma</p>
+              <p id="signUp">
+                Tripma is totally free to use. Sign up using your email address
+                or phone number below to get started.
+              </p>
+            </div>
+            <div className="labels">
+              <input type="text" placeholder="Email or phone number" />
+              <input type="password" placeholder="Password" />
+            </div>
+            <div className="term_conditions">
+              <div className="term_of_condition" >
+                <input type="checkbox" />
+                <p>
+                  I agree to the <span>terms and conditions</span>
+                </p>
+              </div>
+              <div className="term_of_condition" >
+                <input type="checkbox" />
+                <p>Send me the latest deal alerts</p>
+              </div>
+            </div>
+            <button>Create account</button>
+          </div>
+          <div className="separation_line">
+            <div className="divider" />
+            <p>or</p>
+            <div className="divider" />
+          </div>
+          <div className="third_party_auth">
+            <div className="third_party_btn">
+              <img src={googleIcon} alt="" />
+              <p>Continue with Google</p>
+            </div>
+            <div className="third_party_btn">
+              <img src={appleIcon} alt="" />
+              <p>Continue with Apple</p>
+            </div>
+            <div className="third_party_btn">
+              <img src={facebookIcon} alt="" />
+              <p>Continue with Facebook</p>
+            </div>
+          </div>
+        </form>
       </SignUp>
     </div>
   );
--- client/src/Pages/ProfileUser.tsx
+++ client/src/Pages/ProfileUser.tsx
@@ -1,11 +1,34 @@
-import React from 'react'
+import "../styles/ClientProfil.css";
+import React from "react";
+import Navbar from "../Components/Navbar";
+import Footer from "../Components/Footer";
 
-function ProfileUser() {
+const ProfileUser: React.FC = () => {
   return (
-    <div className='userProfile_container'>
-      
-    </div>
-  )
-}
+    <>
+      <Navbar />
+      <div className="userProfile_main_container">
+        <div className="userProfile_container">
+          <div className="profile_side_nav">
+            <div className="side_nav_card">
+              <p className="account_seeting_title">Account Setting</p>
+              <p className="account_seeting_desc">Details about your Personal information</p>
+            </div>
+            <div className="side_nav_card">
+              <p className="account_seeting_title">User checkout</p>
+              <p className="account_seeting_desc">Details about your Payment transactions</p>
+            </div>
+            <div className="side_nav_card">
+              <p className="account_seeting_title">Password & Security</p>
+              <p className="account_seeting_desc">Details about your Personal information</p>
+            </div>
+          </div>
+          <div className="profile_container"></div>
+        </div>
+      </div>
+      {/* <Footer /> */}
+    </>
+  );
+};
 
-export default ProfileUser
+export default ProfileUser;
