ccd9d8f1ccba0bba7571336904f55a55080b7ce6
FediMaalej <fm3aalej@gmail.com>
2023-10-13 00:19:53 +0100

    Payment page completed

--- client/src/Pages/Payment.tsx
+++ client/src/Pages/Payment.tsx
@@ -1,16 +1,13 @@
 import "../styles/Payment.css";
 import React from "react";
-import BagIllustration from "../Assets/Illustration.png";
-import logoIllustration from "../Assets/logoIllustration.png";
 import icon1 from "../Assets/icons/PaymentPage-icons/credit card.svg";
-import icon2 from "../Assets/icons/PaymentPage-icons/grey.svg";
-import icon3 from "../Assets/icons/PaymentPage-icons/apple mac.svg";
-import icon4 from "../Assets/icons/PaymentPage-icons/paypal.svg";
 import icon5 from "../Assets/icons/PaymentPage-icons/flousi.svg";
 import information from "../Assets/icons/PaymentPage-icons/information.svg";
 import googleIcon from "../Assets/icons/thirdPartyIcons/color.svg";
 import appleIcon from "../Assets/icons/thirdPartyIcons/appleMac.svg";
 import facebookIcon from "../Assets/icons/thirdPartyIcons/facebook.svg";
+import BagIllustration from "../Assets/Illustration.png";
+import logoIllustration from "../Assets/logoIllustration.png";
 
 interface PaymentProps {}
 
@@ -32,18 +29,6 @@ const Payment: React.FC<PaymentProps> = () => {
               <p>Credit card</p>
             </li>
             <li>
-              <img src={icon2} alt="" />
-              <p>Google Pay</p>
-            </li>
-            <li>
-              <img src={icon3} alt="" />
-              <p>Apple pay</p>
-            </li>
-            <li>
-              <img src={icon4} alt="" />
-              <p>Paypal</p>
-            </li>
-            <li>
               <img id="flousi_icon" src={icon5} alt="" />
               <p>Flousi</p>
             </li>
@@ -51,17 +36,19 @@ const Payment: React.FC<PaymentProps> = () => {
           <div className="payment_methods_creditCard_details">
             <p>Credit card details</p>
             <div className="payment_methods_billing_adress_check">
-              <input type="checkbox" />
+              <label>
+                <input type="checkbox" />
+              </label>
               <p>Billing address is same as Passenger 1 </p>
             </div>
             <input type="text" placeholder="Name on card" />
             <input type="text" placeholder="Card number" />
             <div className="exp-ccv-inputs">
-              <div>
+              <div className="exp">
                 <input type="text" placeholder="Expiration date" />
                 <p>MM/YY</p>
               </div>
-              <div>
+              <div className="ccv">
                 <input type="text" placeholder="CCV" />
                 <img src={information} alt="" />
               </div>
@@ -84,19 +71,20 @@ const Payment: React.FC<PaymentProps> = () => {
             </div>
             <div className="payment_methods_createAccount_dividers">
               <div className="divider_lines" />
+              <p>or</p>
               <div className="divider_lines" />
             </div>
             <div className="payment_methods_createAccount_third_party_auth">
               <div className="payment_third_party_btn">
-                <img src={googleIcon} alt="" />
+                <img id="firstImg" src={googleIcon} alt="" />
                 <p>Continue with Google</p>
               </div>
               <div className="payment_third_party_btn">
-                <img src={appleIcon} alt="" />
+                <img id="secondtImg" src={appleIcon} alt="" />
                 <p>Continue with Apple</p>
               </div>
               <div className="payment_third_party_btn">
-                <img src={facebookIcon} alt="" />
+                <img id="thirdtImg" src={facebookIcon} alt="" />
                 <p>Continue with Facebook</p>
               </div>
             </div>
@@ -109,8 +97,8 @@ const Payment: React.FC<PaymentProps> = () => {
                 change your flight up to 30 days before the departure date, you
                 are eligible for a free refund. All flights booked on Tripma are
                 backed by our satisfaction guarantee, however cancellation
-                policies vary by airline. See the full{" "}
-                <span>cancellation policy</span> for this flight.
+                policies vary by airline. See the{" "}
+                <span>full cancellation policy</span> for this flight.
               </p>
             </div>
             <div className="payment_methods_privacyPolicy_btns">
@@ -119,13 +107,13 @@ const Payment: React.FC<PaymentProps> = () => {
             </div>
           </div>
         </div>
-        <div className="payment_bills">
-          {/* <div className="payment_bills_trips">
+        {/* <div className="payment_bills">
+          <div className="payment_bills_trips">
             <div className="payment_bills_informatios">
               <div className="payment_bills_trip-info">
                 <div className="payment_bills_aller">
                   <div className="payment_bills_aller_container">
-                    <img src={logoIllustration} alt="" />
+                    <img className="imgIco" src={logoIllustration} alt="" />
                     <div className="payment_bills_img_title">
                       <p>Hawaiian Airlines</p>
                       <p className="payment_bills_ref">FIG4312</p>
@@ -139,7 +127,7 @@ const Payment: React.FC<PaymentProps> = () => {
                 </div>
                 <div className="payment_bills_retour">
                   <div className="payment_bills_retour_container">
-                    <img src={logoIllustration} alt="" />
+                    <img className="imgIco" src={logoIllustration} alt="" />
                     <div className="payment_bills_img_title">
                       <p>Hawaiian Airlines</p>
                       <p className="payment_bills_ref">FIG4312</p>
@@ -167,7 +155,56 @@ const Payment: React.FC<PaymentProps> = () => {
               <button className="payment_bills_btn">Select seats</button>
             </div>
             <img id="illustration" src={BagIllustration} alt="" />
-          </div> */}
+          </div>
+        </div> */}
+        <div className="bills-trips">
+          <div className="informatios">
+            <div className="trip-info">
+              <div className="aller">
+                <div className="aller_container">
+                  <img src={logoIllustration} alt="" />
+                  <div className="img_title">
+                    <p>Hawaiian Airlines</p>
+                    <p className="ref">FIG4312</p>
+                  </div>
+                </div>
+                <div className="time">
+                  <p>16h 45m (+1d)</p>
+                  <p>7:00 AM - 4:15 PM</p>
+                  <p className="ref">2h 45m in HNL</p>
+                </div>
+              </div>
+              <div className="retour">
+                <div className="retour_container">
+                  <img src={logoIllustration} alt="" />
+                  <div className="img_title">
+                    <p>Hawaiian Airlines</p>
+                    <p className="ref">FIG4312</p>
+                  </div>
+                </div>
+                <div className="time">
+                  <p>16h 45m (+1d)</p>
+                  <p>7:00 AM - 4:15 PM</p>
+                  <p className="ref">2h 45m in HNL</p>
+                </div>
+              </div>
+            </div>
+            <div className="trip_fees">
+              <div className="total-cost">
+                <p className="trip_seatPricing">Seat upgrade</p>
+                <p>Subtotal</p>
+                <p>Taxes and Fees</p>
+                <p>Total</p>
+              </div>
+              <div className="prices">
+                <p className="trip_seatPricing">$199</p>
+                <p>$702</p>
+                <p>$66</p>
+                <p>$768</p>
+              </div>
+            </div>
+            <button>Select seats</button>
+          </div>
         </div>
       </div>
     </div>
