155182942a3cf321e64d9e86f16980eec899c527
irma-ahmed <ahmedirmani@yahoo.fr>
2023-10-13 16:09:57 +0100

    delete user done

--- admin/src/Components/Chat.js
+++ admin/src/Components/Chat.js
@@ -1,36 +1,20 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import SideBar from './SideBar';
 import '../styles/Chat.css';
-import { io } from 'socket.io-client';
 
-const users = [
-  { id: 1, name: 'John', email: 'john@example.com', phone: '123-456-7890' },
-  { id: 2, name: 'Jane', email: 'jane@example.com', phone: '234-567-8901' },
-  { id: 3, name: 'Bob', email: 'bob@example.com', phone: '345-678-9012' },
-];
-
-const Chat = () => {
-  const [clicked, setClicked] = useState(true);
+function ChatRoom() {
   const [messages, setMessages] = useState([]);
   const [message, setMessage] = useState('');
-  const [username, setUsername] = useState('');
+  const [clicked, setClicked] = useState(true);
 
-  const socket = io('http://localhost:3000');
-
-  useEffect(() => {
-    socket.on('message', (message) => {
-      setMessages((prevMessages) => [...prevMessages, message]);
-    });
-  }, []);
-
-  const handleSendMessage = () => {
-    socket.emit('message', { text: message, username });
-    setMessage('');
+  const handleMessageChange = (e) => {
+    setMessage(e.target.value);
   };
 
-  const handleUsernameChange = (e) => {
-    const selectedUser = users.find((user) => user.name === e.target.value);
-    setUsername(selectedUser.name);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setMessages([...messages, message]);
+    setMessage('');
   };
 
   return (
@@ -44,88 +28,34 @@ const Chat = () => {
           marginLeft: clicked ? '250px' : '70px',
         }}
       >
-        <div class="container">
-          <div class="row clearfix">
-            <div class="col-lg-12">
-              <div class="card chat-app">
-                <div class="chat">
-                  <div class="chat-header clearfix">
-                    <div class="row">
-                      <div class="col-lg-6">
-                        <div class="chat-about">
-                          <h6 class="m-b-0">Chat Room</h6>
-                        </div>
-                      </div>
-                      <div class="col-lg-6 hidden-sm text-right">
-                        <input
-                          type="text"
-                          class="form-control"
-                          placeholder="Select your username"
-                          list="users"
-                          onChange={handleUsernameChange}
-                        />
-                        <datalist id="users">
-                          {users.map((user) => (
-                            <option value={user.name} key={user.id} />
-                          ))}
-                        </datalist>
-                      </div>
-                    </div>
-                  </div>
-                  <div class="chat-history">
-                    <ul class="m-b-0">
-                      {messages.map((message, index) => (
-                        <li
-                          key={index}
-                          class={`clearfix ${
-                            message.username === username ? 'self' : ''
-                          }`}
-                        >
-                          <div class="message-data">
-                            <span class="message-data-name">
-                              {message.username}
-                            </span>
-                            <span class="message-data-time">
-                              {new Date(message.timestamp).toLocaleTimeString()}
-                            </span>
-                          </div>
-                          <div class="message my-message">
-                            {message.text}
-                          </div>
-                        </li>
-                      ))}
-                    </ul>
-                  </div>
-                  <div class="chat-message clearfix">
-                    <div class="input-group mb-0">
-                      <div class="input-group-prepend">
-                        <span class="input-group-text">
-                          <i class="fa fa-send"></i>
-                        </span>
-                      </div>
-                      <input
-                        type="text"
-                        class="form-control"
-                        placeholder="Enter text here..."
-                        value={message}
-                        onChange={(e) => setMessage(e.target.value)}
-                      />
-                      <button
-                        class="btn btn-primary"
-                        onClick={handleSendMessage}
-                      >
-                        Send
-                      </button>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
+        <div className="chat-room">
+          <div className="chat-room-header">
+            <h2>Chat Room</h2>
+          </div>
+          <div className="chat-room-messages">
+            <ul>
+              {messages.map((message, index) => (
+                <li key={index} className="chat-room-message">
+                  {message}
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div className="chat-room-input">
+            <form onSubmit={handleSubmit}>
+              <input
+                type="text"
+                placeholder="Type your message here"
+                value={message}
+                onChange={handleMessageChange}
+              />
+              <button type="submit">Send</button>
+            </form>
           </div>
         </div>
       </div>
     </div>
   );
-};
+}
 
-export default Chat;
+export default ChatRoom;
--- server/index.js
+++ server/index.js
@@ -1,44 +1,34 @@
 const express = require('express');
-const cors = require('cors');
-const http = require('http');
-const socketio = require('socket.io');
-const userRoutes = require('./router/user.routes.js');
-const adminRoutes = require('./router/admin.router.js');
-const flightsRoutes = require('./router/flights.router.js');
-const seatsRoutes = require('./router/seats.routes.js');
-const ReservationRoutes = require('./router/reservation.routes.js');
-const paymentRoutes = require('./router/payment.routes.js');
-require('./database/index.js');
-
+const cors = require('cors')
 const app = express();
 const port = 1128; // You can choose any available port
+const userRoutes = require("./router/user.routes.js")
+const adminRoutes = require("./router/admin.router.js")
+const flightsRoutes = require("./router/flights.router.js")
 
+const seatsRoutes = require("./router/seats.routes.js")
+const ReservationRoutes = require("./router/reservation.routes.js")
+
+const paymentRoutes = require("./router/payment.routes.js")
+require("./database/index.js")
 app.use(express.json());
-app.use(cors());
+app.use(cors())
+// Define a route
 
-const server = http.createServer(app);
-const io = socketio(server);
 
-io.on('connection', (socket) => {
-  console.log('New client connected');
+app.use("/users", userRoutes)
+app.use("/admin", adminRoutes)
+app.use("/reservation",ReservationRoutes)
+app.use("/payment",paymentRoutes)
 
-  socket.on('message', (message) => {
-    console.log(`Received message: ${message}`);
-    io.emit('message', message);
-  });
 
-  socket.on('disconnect', () => {
-    console.log('Client disconnected');
-  });
-});
 
-app.use('/users', userRoutes);
-app.use('/admin', adminRoutes);
-app.use('/reservation', ReservationRoutes);
-app.use('/payment', paymentRoutes);
-app.use('/flights', flightsRoutes);
-app.use('/seats', seatsRoutes);
 
-server.listen(port, () => {
+
+app.use("/flights",flightsRoutes)
+app.use("/seats", seatsRoutes)
+
+// Start the server
+app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
