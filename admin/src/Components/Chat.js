3cb0c0e02286cb873513c0892e64352476676476
irma-ahmed <ahmedirmani@yahoo.fr>
2023-10-13 14:08:21 +0100

    updating

--- admin/src/Components/Chat.js
+++ admin/src/Components/Chat.js
@@ -1,11 +1,37 @@
-import React from 'react'
-import { useState } from 'react'
+import React, { useState, useEffect } from 'react';
 import SideBar from './SideBar';
 import '../styles/Chat.css';
+import { io } from 'socket.io-client';
 
+const users = [
+  { id: 1, name: 'John', email: 'john@example.com', phone: '123-456-7890' },
+  { id: 2, name: 'Jane', email: 'jane@example.com', phone: '234-567-8901' },
+  { id: 3, name: 'Bob', email: 'bob@example.com', phone: '345-678-9012' },
+];
 
 const Chat = () => {
-    const [clicked, setClicked] = useState(true);
+  const [clicked, setClicked] = useState(true);
+  const [messages, setMessages] = useState([]);
+  const [message, setMessage] = useState('');
+  const [username, setUsername] = useState('');
+
+  const socket = io('http://localhost:3000');
+
+  useEffect(() => {
+    socket.on('message', (message) => {
+      setMessages((prevMessages) => [...prevMessages, message]);
+    });
+  }, []);
+
+  const handleSendMessage = () => {
+    socket.emit('message', { text: message, username });
+    setMessage('');
+  };
+
+  const handleUsernameChange = (e) => {
+    const selectedUser = users.find((user) => user.name === e.target.value);
+    setUsername(selectedUser.name);
+  };
 
   return (
     <div style={{ display: 'flex' }}>
@@ -18,123 +44,88 @@ const Chat = () => {
           marginLeft: clicked ? '250px' : '70px',
         }}
       >
-        <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" />
-
-<div class="container">
-<div class="row clearfix">
-    <div class="col-lg-12">
-        <div class="card chat-app">
-            <div id="plist" class="people-list">
-                <div class="input-group">
-                    <div class="input-group-prepend">
-                        <span class="input-group-text"><i class="fa fa-search"></i></span>
-                    </div>
-                    <input type="text" class="form-control" placeholder="Search..."/>
-                </div>
-                <ul class="list-unstyled chat-list mt-2 mb-0">
-                    <li class="clearfix">
-                        <img src="https://bootdey.com/img/Content/avatar/avatar1.png" alt="avatar"/>
-                        <div class="about">
-                            <div class="name">Vincent Porter</div>
-                            <div class="status"> <i class="fa fa-circle offline"></i> left 7 mins ago </div>                                            
-                        </div>
-                    </li>
-                    <li class="clearfix active">
-                        <img src="https://bootdey.com/img/Content/avatar/avatar2.png" alt="avatar"/>
-                        <div class="about">
-                            <div class="name">Aiden Chavez</div>
-                            <div class="status"> <i class="fa fa-circle online"></i> online </div>
-                        </div>
-                    </li>
-                    <li class="clearfix">
-                        <img src="https://bootdey.com/img/Content/avatar/avatar3.png" alt="avatar"/>
-                        <div class="about">
-                            <div class="name">Mike Thomas</div>
-                            <div class="status"> <i class="fa fa-circle online"></i> online </div>
-                        </div>
-                    </li>                                    
-                    <li class="clearfix">
-                        <img src="https://bootdey.com/img/Content/avatar/avatar7.png" alt="avatar"/>
-                        <div class="about">
-                            <div class="name">Christian Kelly</div>
-                            <div class="status"> <i class="fa fa-circle offline"></i> left 10 hours ago </div>
-                        </div>
-                    </li>
-                    <li class="clearfix">
-                        <img src="https://bootdey.com/img/Content/avatar/avatar8.png" alt="avatar"/>
-                        <div class="about">
-                            <div class="name">Monica Ward</div>
-                            <div class="status"> <i class="fa fa-circle online"></i> online </div>
-                        </div>
-                    </li>
-                    <li class="clearfix">
-                        <img src="https://bootdey.com/img/Content/avatar/avatar3.png" alt="avatar"/>
-                        <div class="about">
-                            <div class="name">Dean Henry</div>
-                            <div class="status"> <i class="fa fa-circle offline"></i> offline since Oct 28 </div>
-                        </div>
-                    </li>
-                </ul>
-            </div>
-            <div class="chat">
-                <div class="chat-header clearfix">
+        <div class="container">
+          <div class="row clearfix">
+            <div class="col-lg-12">
+              <div class="card chat-app">
+                <div class="chat">
+                  <div class="chat-header clearfix">
                     <div class="row">
-                        <div class="col-lg-6">
-                            <a href="javascript:void(0);" data-toggle="modal" data-target="#view_info">
-                                <img src="https://bootdey.com/img/Content/avatar/avatar2.png" alt="avatar"/>
-                            </a>
-                            <div class="chat-about">
-                                <h6 class="m-b-0">Aiden Chavez</h6>
-                                <small>Last seen: 2 hours ago</small>
-                            </div>
+                      <div class="col-lg-6">
+                        <div class="chat-about">
+                          <h6 class="m-b-0">Chat Room</h6>
                         </div>
-                        <div class="col-lg-6 hidden-sm text-right">
-                            <a href="javascript:void(0);" class="btn btn-outline-secondary"><i class="fa fa-camera"></i></a>
-                            <a href="javascript:void(0);" class="btn btn-outline-primary"><i class="fa fa-image"></i></a>
-                            <a href="javascript:void(0);" class="btn btn-outline-info"><i class="fa fa-cogs"></i></a>
-                            <a href="javascript:void(0);" class="btn btn-outline-warning"><i class="fa fa-question"></i></a>
-                        </div>
+                      </div>
+                      <div class="col-lg-6 hidden-sm text-right">
+                        <input
+                          type="text"
+                          class="form-control"
+                          placeholder="Select your username"
+                          list="users"
+                          onChange={handleUsernameChange}
+                        />
+                        <datalist id="users">
+                          {users.map((user) => (
+                            <option value={user.name} key={user.id} />
+                          ))}
+                        </datalist>
+                      </div>
                     </div>
-                </div>
-                <div class="chat-history">
+                  </div>
+                  <div class="chat-history">
                     <ul class="m-b-0">
-                        <li class="clearfix">
-                            <div class="message-data text-right">
-                                <span class="message-data-time">10:10 AM, Today</span>
-                                <img src="https://bootdey.com/img/Content/avatar/avatar7.png" alt="avatar"/>
-                            </div>
-                            <div class="message other-message float-right"> Hi Aiden, how are you? How is the project coming along? </div>
+                      {messages.map((message, index) => (
+                        <li
+                          key={index}
+                          class={`clearfix ${
+                            message.username === username ? 'self' : ''
+                          }`}
+                        >
+                          <div class="message-data">
+                            <span class="message-data-name">
+                              {message.username}
+                            </span>
+                            <span class="message-data-time">
+                              {new Date(message.timestamp).toLocaleTimeString()}
+                            </span>
+                          </div>
+                          <div class="message my-message">
+                            {message.text}
+                          </div>
                         </li>
-                        <li class="clearfix">
-                            <div class="message-data">
-                                <span class="message-data-time">10:12 AM, Today</span>
-                            </div>
-                            <div class="message my-message">Are we meeting today?</div>                                    
-                        </li>                               
-                        <li class="clearfix">
-                            <div class="message-data">
-                                <span class="message-data-time">10:15 AM, Today</span>
-                            </div>
-                            <div class="message my-message">Project has been already finished and I have results to show you.</div>
-                        </li>
+                      ))}
                     </ul>
-                </div>
-                <div class="chat-message clearfix">
+                  </div>
+                  <div class="chat-message clearfix">
                     <div class="input-group mb-0">
-                        <div class="input-group-prepend">
-                            <span class="input-group-text"><i class="fa fa-send"></i></span>
-                        </div>
-                        <input type="text" class="form-control" placeholder="Enter text here..."/>                                    
+                      <div class="input-group-prepend">
+                        <span class="input-group-text">
+                          <i class="fa fa-send"></i>
+                        </span>
+                      </div>
+                      <input
+                        type="text"
+                        class="form-control"
+                        placeholder="Enter text here..."
+                        value={message}
+                        onChange={(e) => setMessage(e.target.value)}
+                      />
+                      <button
+                        class="btn btn-primary"
+                        onClick={handleSendMessage}
+                      >
+                        Send
+                      </button>
                     </div>
+                  </div>
                 </div>
+              </div>
             </div>
+          </div>
         </div>
+      </div>
     </div>
-</div>
-</div>
-    </div>
-    </div>
-  )
-}
+  );
+};
 
-export default Chat
+export default Chat;
--- server/index.js
+++ server/index.js
@@ -1,34 +1,44 @@
 const express = require('express');
-const cors = require('cors')
+const cors = require('cors');
+const http = require('http');
+const socketio = require('socket.io');
+const userRoutes = require('./router/user.routes.js');
+const adminRoutes = require('./router/admin.router.js');
+const flightsRoutes = require('./router/flights.router.js');
+const seatsRoutes = require('./router/seats.routes.js');
+const ReservationRoutes = require('./router/reservation.routes.js');
+const paymentRoutes = require('./router/payment.routes.js');
+require('./database/index.js');
+
 const app = express();
 const port = 1128; // You can choose any available port
-const userRoutes = require("./router/user.routes.js")
-const adminRoutes = require("./router/admin.router.js")
-const flightsRoutes = require("./router/flights.router.js")
 
-const seatsRoutes = require("./router/seats.routes.js")
-const ReservationRoutes = require("./router/reservation.routes.js")
-
-const paymentRoutes = require("./router/payment.routes.js")
-require("./database/index.js")
 app.use(express.json());
-app.use(cors())
-// Define a route
+app.use(cors());
 
+const server = http.createServer(app);
+const io = socketio(server);
 
-app.use("/users", userRoutes)
-app.use("/admin", adminRoutes)
-app.use("/reservation",ReservationRoutes)
-app.use("/payment",paymentRoutes)
+io.on('connection', (socket) => {
+  console.log('New client connected');
 
+  socket.on('message', (message) => {
+    console.log(`Received message: ${message}`);
+    io.emit('message', message);
+  });
 
+  socket.on('disconnect', () => {
+    console.log('Client disconnected');
+  });
+});
 
+app.use('/users', userRoutes);
+app.use('/admin', adminRoutes);
+app.use('/reservation', ReservationRoutes);
+app.use('/payment', paymentRoutes);
+app.use('/flights', flightsRoutes);
+app.use('/seats', seatsRoutes);
 
-
-app.use("/flights",flightsRoutes)
-app.use("/seats", seatsRoutes)
-
-// Start the server
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
